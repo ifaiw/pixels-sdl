d6433e183804ef0106be23108a7594b333cb0247
Mattt Enss <matthew.enss@carta.com>
2024-06-06 20:36:16 -0400

    Happy with 60fps, only use micros implememntation

--- main.cpp
+++ main.cpp
@@ -4,6 +4,8 @@
 // g++ main.cpp -o prog.exe -lmingw32 -lSDL2main -lSDL2
 
 #include <assert.h>
+#include <time.h>
+#include <unistd.h>
 
 // For printing out uints
 #include <inttypes.h>
@@ -44,25 +46,70 @@ static void to_byte_str32(uint32_t b, int buffer_index, char* o_buffer) {
     }
 }
 
+#define WIDTH 1280
+#define HEIGHT 960
+#define FRAMES_PER_SECOND 60
+#define SLEEP_BUFFER_MICROS 1000
+#define X_KEYPRESS_SPEED 15
 #define RED   0x00FF0000
 #define GREEN 0x0000FF00
 #define BLUE  0x000000FF
 #define ALPHA 0xFF000000
+#define RED_SHIFT   16
+#define GREEN_SHIFT  8
+#define BLUE_SHIFT   0
 
-// Assume 640x480
-static void set_some_colours(uint32_t* pixels) {
+static inline long diff_time_in_nanos(struct timespec *t1, struct timespec *t2) {
+    long diff_in_nanos_from_sec = (t2->tv_sec - t1->tv_sec) * (long)1000000000;
+    long diff_in_nanos_from_nanos = t2->tv_nsec - t1->tv_nsec;
+    return diff_in_nanos_from_sec + diff_in_nanos_from_nanos;
+}
+
+static inline long diff_time_in_micros(struct timespec *t1, long t2_secs, long t2_nanos) {
+    long diff_in_micros_from_sec = (t2_secs - t1->tv_sec) * (long)1000000;
+    long diff_in_micros_from_nanos = (t2_nanos - t1->tv_nsec) / (long)1000;
+    return diff_in_micros_from_sec + diff_in_micros_from_nanos;
+}
+
+static inline long micros_until_next_frame(struct timespec *initial, struct timespec *current, long next_frame_count, long nanos_per_frame) {
+    long nanos_of_next_frame_since_initial = next_frame_count * nanos_per_frame;
+    long seconds_of_next_frame_since_initial = nanos_of_next_frame_since_initial / (long)1000000000;
+    long nanos_remainder = nanos_of_next_frame_since_initial - (seconds_of_next_frame_since_initial * (long)1000000000);
+
+    long seconds_next_frame = initial->tv_sec + seconds_of_next_frame_since_initial;
+    long nanos_next_frame = initial->tv_nsec + nanos_remainder;
+
+    return diff_time_in_micros(current, seconds_next_frame, nanos_next_frame);
+}
+
+static inline long micros_until_next_frame(long initial_micros, long current_micros, long next_frame_count, long micros_per_frame) {
+    long micros_of_next_frame_since_initial = next_frame_count * micros_per_frame;
+    long micros_of_next_frame = micros_of_next_frame_since_initial + initial_micros;
+    return micros_of_next_frame - current_micros;
+}
+
+static inline long to_micros(struct timespec *t1) {
+    long micros_from_secs = t1->tv_sec * (long)1000000;
+    long micros_from_nanos = t1->tv_nsec / (long)1000;
+    return micros_from_secs + micros_from_nanos;
+}
+
+
+static void set_some_colours(uint32_t* pixels, int width, int height, int x_offset) {
     // Three horizontal colour bands
     int i = 0;
-    for (int y = 0; y < 480; ++y) {
-        for (int x = 0; x < 640; ++x) {
-            if (y < 480/3) {
-                pixels[i] = RED;
+
+    for (int y = 0; y < height; ++y) {
+        for (int x = 0; x < width; ++x) {
+            uint32_t colour_level = ((x_offset + x) % 255);
+            if (y < height/3) {
+                pixels[i] = colour_level << RED_SHIFT | ALPHA;
             }
-            else if (y < 480 * 2 / 3) {
-                pixels[i] = GREEN | ALPHA;
+            else if (y < height * 2 / 3) {
+                pixels[i] = colour_level << GREEN_SHIFT | ALPHA;
             }
             else {
-                pixels[i] = BLUE | ALPHA;
+                pixels[i] = colour_level << BLUE_SHIFT | ALPHA;
             }
             i += 1;
         }
@@ -93,51 +140,18 @@ int main(int argc, char* argv[]){
     window = SDL_CreateWindow("C++ SDL2 Window",
             20,
             20,
-            640,
-            480,
+            WIDTH,
+            HEIGHT,
             SDL_WINDOW_SHOWN);
 
     screen = SDL_GetWindowSurface(window);
 
-    printf("About to load image\n");
-    // SDL_Surface *image = IMG_Load("/Users/matthewenss/personal/c/assets/10x10-custom.png");
+    uint32_t pixels[HEIGHT * WIDTH];
+    int x_offset = 0;
+    int x_speed = 0;
 
-    //SDL_Surface* image;
-//    image = SDL_LoadBMP("/Users/matthewenss/personal/c/assets/3D-Window_BBB_Blender_2.bmp");
-    //image = SDL_LoadBMP("/Users/matthewenss/personal/c/assets/10x10-custom.bmp");
-    //image = SDL_LoadBMP("/Users/matthewenss/personal/c/assets/10x10-custom-from-png.bmp");
-    // image = SDL_LoadBMP("/Users/matthewenss/personal/c/assets/bmp-test.bmp");
-    //printf("Image loaded\n");
-
-    /*std::cout << "The pitch is " << image->pitch << "\n";
-    std::cout << "width=" << image->w << " height=" << image->h << "\n";
-    std::cout << "sizeof pixels is " << sizeof(image->pixels) << "\n";
-
-    SDL_PixelFormat* format = image->format;
-    printf("Bits per pixel is %" PRIu8 "\n", format->BitsPerPixel);
-    printf("Bytes per pixel is %" PRIu8 "\n", format->BytesPerPixel);
-    char s[49];
-    printf("sizeof pixel format is %lu\n", sizeof(format->format));
-    to_byte_str32(format->format, 0, s);
-    printf("Format is %s\n", s);
-
-    uint32_t* pixels = (uint32_t*)image->pixels;
-    //for (int i = 0; i < (image->w * image->h); ++i) {
-    for (int i = 0; i < 11; ++i) {
-        printf("pixel %i is %" PRIu32 "\n", i, pixels[i]);
-        s[48] = 0;
-        to_byte_str32(pixels[i], 0, s);
-        printf("hex is %s\n", s);
-    }*/
-
-    uint32_t pixels[480 * 640];
-    set_some_colours(pixels);
-     SDL_Surface *surface = SDL_CreateRGBSurfaceFrom((void*)pixels, 640, 480, 32, 640 * 32 / 8, RED, GREEN, BLUE, ALPHA);
-    //SDL_Surface *surface = SDL_CreateRGBSurfaceFrom((void*)pixels, 20, 20, 32, 20, RED, GREEN, BLUE, ALPHA);
-    printf("pixels[1] %" PRIu32 "\n", pixels[1]);
-    printf("pixels[640*200] %" PRIu32 "\n", pixels[640 * 200]);
-    printf("pixels[640*480-1] %" PRIu32 "\n", pixels[(640 * 480) - 1]);
-
+    set_some_colours(pixels, WIDTH, HEIGHT, x_offset);
+    SDL_Surface *surface = SDL_CreateRGBSurfaceFrom((void*)pixels, WIDTH, HEIGHT, 32, WIDTH * 32 / 8, RED, GREEN, BLUE, ALPHA);
     SDL_BlitSurface(surface,NULL,screen,NULL);
     SDL_FreeSurface(surface);
     SDL_UpdateWindowSurface(window);
@@ -145,10 +159,41 @@ int main(int argc, char* argv[]){
     // Infinite loop for our application
     bool gameIsRunning = true;
     // Main application loop
+    struct timespec time_at_top;
+    struct timespec time_at_bottom;
+    struct timespec first_time_at_top;
+    long time_at_top_micros;
+    long time_at_bottom_micros;
+    long first_micros_at_top;
+
+    // Not used anymore? long nanos_per_frame = (long)1000000000 / (long)FRAMES_PER_SECOND;
+    // printf("nanos_per_frame is %ld\n", nanos_per_frame);
+    long micros_per_frame = (long)1000000 / (long)FRAMES_PER_SECOND;
+    printf("micros_per_frame is %ld", micros_per_frame);
+    long total_frame_count = 0;
+    clock_gettime(CLOCK_REALTIME, &first_time_at_top);
+    printf("first_time_at_top is %ld.%ld\n", first_time_at_top.tv_sec, first_time_at_top.tv_nsec);
+    first_micros_at_top = to_micros(&first_time_at_top);
+    printf("first_micros_at_top is %ld\n", first_micros_at_top);
+
+    long target_time = first_micros_at_top;
+
     while(gameIsRunning){
+        clock_gettime(CLOCK_REALTIME, &time_at_top);
+        time_at_top_micros = to_micros(&time_at_top);
+        printf("Current time %ld.%ld\n", time_at_top.tv_sec, time_at_top.tv_nsec);
+        printf("time_at_top_micros is %ld\n", time_at_top_micros);
+
+        long target_miss = time_at_top_micros - target_time;
+        printf("miss by %ld\n", target_miss);
+
+        if (total_frame_count % 60 == 0) {
+            printf("Frame %ld at %ld\n", total_frame_count, time_at_top_micros);
+        }
         SDL_Event event;
         // Start our event loop
         while(SDL_PollEvent(&event)){
+            printf("event type is %i\n", event.type);
             // Handle each specific event
             if(event.type == SDL_QUIT){
                 gameIsRunning= false;
@@ -163,9 +208,36 @@ int main(int argc, char* argv[]){
             const Uint8* state = SDL_GetKeyboardState(NULL);
             if(state[SDL_SCANCODE_RIGHT]){
                 std::cout << "right arrow key is pressed\n";
+                x_speed += 2;
+            }
+            else if (state[SDL_SCANCODE_LEFT]){
+                std::cout << "left arrow key is pressed\n";
+                x_speed -= 2;
             }
         }
 
+        x_offset += x_speed;
+
+        set_some_colours(pixels, WIDTH, HEIGHT, x_offset);
+        SDL_Surface *surface = SDL_CreateRGBSurfaceFrom((void*)pixels, WIDTH, HEIGHT, 32, WIDTH * 32 / 8, RED, GREEN, BLUE, ALPHA);
+        SDL_BlitSurface(surface,NULL,screen,NULL);
+        SDL_FreeSurface(surface);
+        SDL_UpdateWindowSurface(window);
+ 
+        total_frame_count++;
+
+        clock_gettime(CLOCK_REALTIME, &time_at_bottom);
+        time_at_bottom_micros = to_micros(&time_at_bottom);
+        //long micros_to_sleep = micros_until_next_frame(first_micros_at_top, time_at_bottom_micros, total_frame_count, micros_per_frame);
+        target_time = (total_frame_count * micros_per_frame) + first_micros_at_top;
+        long micros_to_sleep = target_time - time_at_bottom_micros - SLEEP_BUFFER_MICROS;
+        // long nanos_elapsed = diff_time(&time_at_top, &time_at_bottom);
+        // long nanos_to_sleep = (nanos_per_frame - nanos_elapsed - SLEEP_BUFFER);
+        // printf("time elapsed in nanos is %ld time to sleep in nanos is %ld\n", nanos_elapsed, nanos_to_sleep);
+        printf("time elapsed in micros is %ld time to sleep in micros is %ld\n", (time_at_bottom_micros - time_at_top_micros), micros_to_sleep);
+        if (micros_to_sleep > 0) {
+           usleep(micros_to_sleep);
+        }
     }
 
     // We destroy our window. We are passing in the pointer
