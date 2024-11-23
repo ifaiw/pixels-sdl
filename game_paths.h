7ac792dc0d4aa517ccd995b05133ab1f64cc31f7
Mattt Enss <matthew.enss@carta.com>
2024-11-23 10:08:45 -0500

    Uses pixels-sdl-assets now

--- game_paths.h
+++ game_paths.h
@@ -4,17 +4,17 @@
 // For work laptop but untested #define GAME_PATH__ROOT_PATH "/Users/matthew.enss/personal/c/"
 #define GAME_PATH__ROOT_PATH "/Users/matthewenss/c/"
 
-#define GAME_PATH__IMAGE_PATH_SOLIDS_1 "assets/used-in-game/Tileset.bmp"
-#define GAME_PATH__IMAGE_PATH_ORC_1_RIGHT "assets/tilesets/Tiny RPG Character Asset Pack v1.02 -Free Soldier&Orc/Orc/Orc/Orc.bmp"
-#define GAME_PATH__IMAGE_PATH_MUSHROOM_RIGHT "assets/used-in-game/mushroom_spritesheet.bmp"
-#define GAME_PATH__IMAGE_PATH_CAT_RIGHT "assets/used-in-game/cato-walk.bmp"
+#define GAME_PATH__IMAGE_PATH_SOLIDS_1 "pixels-sdl-assets/used-in-game/Tileset.bmp"
+#define GAME_PATH__IMAGE_PATH_ORC_1_RIGHT "pixels-sdl-assets/used-in-game/Orc.bmp"
+#define GAME_PATH__IMAGE_PATH_MUSHROOM_RIGHT "pixels-sdl-assets/used-in-game/mushroom_spritesheet.bmp"
+#define GAME_PATH__IMAGE_PATH_CAT_RIGHT "pixels-sdl-assets/used-in-game/cato-walk.bmp"
 
 #define GAME_PATH__IMAGE_PATH_SOLIDS_1_FULL GAME_PATH__ROOT_PATH GAME_PATH__IMAGE_PATH_SOLIDS_1
 #define GAME_PATH__IMAGE_PATH_ORC_1_RIGHT_FULL GAME_PATH__ROOT_PATH GAME_PATH__IMAGE_PATH_ORC_1_RIGHT
 #define GAME_PATH__IMAGE_PATH_MUSHROOM_RIGHT_FULL GAME_PATH__ROOT_PATH GAME_PATH__IMAGE_PATH_MUSHROOM_RIGHT
 #define GAME_PATH__IMAGE_PATH_CAT_RIGHT_FULL GAME_PATH__ROOT_PATH GAME_PATH__IMAGE_PATH_CAT_RIGHT
 
-#define GAME_PATH__TEXT_PATH_WORLD_RULES "assets/world-rules.cfg"
+#define GAME_PATH__TEXT_PATH_WORLD_RULES "pixels-sdl-assets/used-in-game/world-rules.cfg"
 
 #define GAME_PATH__TEXT_PATH_WORLD_RULES_FULL GAME_PATH__ROOT_PATH GAME_PATH__TEXT_PATH_WORLD_RULES
 
